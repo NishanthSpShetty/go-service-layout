cef57d16ca4ca9d3e0d5bff9f49c67714e50678b
NishanthSpShetty <nishanthspshetty@gmail.com>
2022-05-09 20:24:54 +0530

    add guidelines

--- main.go
+++ main.go
@@ -2,5 +2,20 @@ package main
 
 func main() {
 	//put everything here
+
+	//doesnt mean write every bit of code here.
+	//
+	//break them into functions,
+	//
+	//break them into packages as per the domain or some collective grouping of the resposnibility
+	//
+	//provide good name for package, functions for future you and others.
+	//
+	//avoid writing code in generic package such as util, common unless its necessory.
+	//
+	//if you dont find the right package name to put your function, write it in same pacakge where it is used, its okay to duplicate sometime.
+	//
+	//good  package names help in discoverability
+
 	println("it will work")
 }
